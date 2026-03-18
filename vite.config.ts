208f3be239f14b7836a69e8e8058c4367f36ecde
Anupkumar sheshrao koturwar <118750899+Anup-2002@users.noreply.github.com>
2026-03-18 17:35:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      allowedHosts: 'all' 
+      allowedHosts: ["portofolio-anupkumar.onrender.com"]
     },
     plugins: [react()],
     define: {
